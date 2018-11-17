ffba469d6f7e1914d7ed080388c0d5aa7c8cd4aa
Simone <simonetennis33@gmail.com>
2018-11-17 11:48:30 +0100

    nuovo stile

--- cosmoproject/planetario/js/scripts.js
+++ cosmoproject/planetario/js/scripts.js
@@ -23,6 +23,7 @@ $(window).load(function(){
     e.preventDefault();
   });
 
+  //cambia pianeta selezionato
   $("#data a").click(function(e) {
     var ref = $(this).attr("class");
     solarsys.removeClass().addClass(ref);
@@ -30,6 +31,79 @@ $(window).load(function(){
     $(this).addClass('active');
     e.preventDefault();
   });
+
+jQuery(function($) {
+    $('#golink1').click(function() {
+        return false;
+    }).dblclick(function() {
+        window.location = this.href;
+        return false;
+    });
+});
+
+jQuery(function($) {
+  $('#golink2').click(function() {
+      return false;
+  }).dblclick(function() {
+      window.location = this.href;
+      return false;
+  });
+});
+
+jQuery(function($) {
+  $('#golink3').click(function() {
+      return false;
+  }).dblclick(function() {
+      window.location = this.href;
+      return false;
+  });
+});
+
+jQuery(function($) {
+$('#golink4').click(function() {
+    return false;
+}).dblclick(function() {
+    window.location = this.href;
+    return false;
+});
+});
+
+jQuery(function($) {
+  $('#golink5').click(function() {
+      return false;
+  }).dblclick(function() {
+      window.location = this.href;
+      return false;
+  });
+});
+
+jQuery(function($) {
+$('#golink6').click(function() {
+    return false;
+}).dblclick(function() {
+    window.location = this.href;
+    return false;
+});
+});
+
+jQuery(function($) {
+  $('#golink7').click(function() {
+      return false;
+  }).dblclick(function() {
+      window.location = this.href;
+      return false;
+  });
+});
+
+jQuery(function($) {
+$('#golink8').click(function() {
+    return false;
+}).dblclick(function() {
+    window.location = this.href;
+    return false;
+});
+});
+
 
   $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
   $(".set-zoom").click(function() { body.toggleClass("zoom-large zoom-close"); });
