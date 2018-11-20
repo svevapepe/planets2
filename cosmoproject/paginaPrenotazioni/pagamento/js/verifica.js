154bdcbc34bd5de4787211aebf6ae7a19a4d3137
sveva <sveva@linux>
2018-11-20 16:05:53 +0100

    j2

---
+++ cosmoproject/paginaPrenotazioni/pagamento/js/verifica.js
@@ -0,0 +1,23 @@
+function verifica(){
+	var mese=document.myForm3.MM.value;
+	var anno=document.myForm3.MM.value;
+	var numCart=document.myForm3.cardNumber.value;
+	var id=document.myForm3.username.value;
+	if(mese <0 || anno <0){
+		window.alert("La data di scadenza non deve essere negativa");
+		return false;
+	}
+	if(mese>12){
+		window.alert("I mesi sono al massimo 12");
+		return false;
+	}
+	if(isNaN(parseInt(numCart))){
+		window.alert("Il numero della carta deve essere un numero");
+		return false;
+	}
+	if(!isNaN(parseInt(id))){
+		window.alert("Intestatario non può essere un numero");
+		return false;
+	}
+	return true;
+}
